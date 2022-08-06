a3f26d2a843af5e6efc183d25c3c19830c2925ae
EnthernetCode <Enthernetcode@gmai.com>
2022-08-06 21:25:54 +0100

    1st

--- traffickbot.py
+++ traffickbot.py
@@ -8,33 +8,6 @@ visit = 0
 num = float(input('number of times to execute traffic: '))
 while visit < num:
  visit += 1
-# p = termux-open-url
-# chrome_path = '/data/data/com.termux/files/home/python_projects/Chrome.app'
  webbrowser.open(f'{link}')
  time.sleep(2)
-# webbrowser.open_new_tab(f'{link}')
-# time.sleep(2)
 
-
-
-
-# sys.exit()
-
-
-
-#while True:
-#  webbrowser.open(f'{link}')
-#  time.sleep(15)
-#	webbrowser.open_new_tab(f'{link}')
-#	time.sleep(10)
-#	webbrowser.open_new_tab(f'{link}')
-#	time.sleep(10)	
-#	webbrowser.open_new_tab(f'{link}')
-#	time.sleep(10)
-#	webbrowser.open_new_tab(f'{link}')
-#	time.sleep(10)	
-#	webbrowser.open_new_tab(f'{link}')
-#	time.sleep(10)
-#	x +=1
-
-#while visit > num
